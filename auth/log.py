49b6eecdf83f382e7ddec8d3d25bf438615ccb19
JL Houss <tkthoussam@gmail.com>
2022-11-21 17:36:20 +0100

    adding auth folder to dev branch

---
+++ auth/log.py
@@ -0,0 +1,8 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+flow = InstalledAppFlow.from_client_secrets_file('creds.json', SCOPES)
+creds = flow.run_local_server(port=0)
+# Save the credentials for the next run
+with open('token.json', 'w') as token:
+    token.write(creds.to_json())
